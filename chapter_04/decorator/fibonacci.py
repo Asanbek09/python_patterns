055aa3032e2570df578f5104b0dcb3ef96db3e81
Asanbek09 <samudinua@gmail.com>
2025-05-21 16:07:59 +0600

    Завершение примера с Фибоначчи

--- chapter_04/decorator/fibonacci.py
+++ chapter_04/decorator/fibonacci.py
@@ -1,3 +1,12 @@
+sum_cache = {0: 0}
+
+def number_sum(n):
+    if n in sum_cache:
+        return sum_cache[n]
+    res = n + number_sum(n - 1)
+    sum_cache[n] = res
+    return res
+
 fib_cache = {0: 0, 1: 1}
 
 def fibonacci(n):
@@ -7,3 +16,12 @@ def fibonacci(n):
     res = fibonacci(n - 1) + fibonacci(n - 2)
     fib_cache[n] = res
     return res
+
+if __name__ == "__main__":
+    from timeit import Timer
+
+    t = Timer(
+        "fibonacci(100)",
+        "from __main__ import fibonacci",
+    )
+    print("Time: ", t.timeit())
