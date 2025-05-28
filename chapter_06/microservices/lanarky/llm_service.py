14c70ec586b415484633f7ac229d89a190d38baa
Asanbek09 <samudinua@gmail.com>
2025-05-28 15:30:59 +0600

    MVC Pattern через lanarky

---
+++ chapter_06/microservices/lanarky/llm_service.py
@@ -0,0 +1,22 @@
+import os
+
+import uvicorn
+from lanarky import Lanarky
+from lanarky.adapters.openai.resources import ChatCompletionResource
+from lanarky.adapters.openai.routing import OpenAIAPIRouter
+
+os.environ["OPENAI_API_KEY"] = "Your OpenAI API key here"
+
+app = Lanarky()
+router = OpenAIAPIRouter()
+
+
+@router.post("/chat")
+def chat(stream: bool = True) -> ChatCompletionResource:
+    system = "Here is your assistant"
+    return ChatCompletionResource(stream=stream, system=system)
+
+
+if __name__ == "__main__":
+    app.include_router(router)
+    uvicorn.run(app)
